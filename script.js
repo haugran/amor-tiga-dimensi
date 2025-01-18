217b1903eb1b886f04101dd7bde28d1b3fd1606e
Irfan Anshari <irfananshr.id@gmail.com>
2025-01-18 19:03:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 let container = document.querySelector('.container');
 
-window.addEventListener('mousemove',(e)=>{
-  
-  x = e.pageX;
-  y = e.pageY;
-  
-  container.style.animation = "none";
-  container.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
-})
-
-window.addEventListener('mouseout',(e)=>{
-  container.style.animation = "animate 45s linear infinite";
-})
-
- window.addEventListener("touchmove",(e)=>{
-  
-      var x = e.touches[0].pageX;
-      var y = e.touches[0].pageY;
-  
-  container.style.animation = "none";
-  container.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
-  
-})
-
-window.addEventListener('touchend',(e)=>{
-  container.style.animation = "animate 45s linear infinite";
-})
-
-container.addEventListener('mouseover',()=>{
-  
+// Fungsi untuk memunculkan elemen love
+function showLove() {
   document.querySelector('.love').style.opacity = "1";
-})
+  document.querySelector('.love-back').style.opacity = "1";
+}
+
+// Event untuk mouse move
+window.addEventListener('mousemove', (e) => {
+  let x = e.pageX / window.innerWidth * 360;
+  let y = e.pageY / window.innerHeight * 360;
+  container.style.animation = "none";
+  container.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+  showLove();
+});
+
+// Event untuk mouse out
+window.addEventListener('mouseout', () => {
+  container.style.animation = "animate 45s linear infinite";
+});
+
+// Event untuk touch move
+window.addEventListener('touchmove', (e) => {
+  let touch = e.touches[0];
+  let x = touch.pageX / window.innerWidth * 360;
+  let y = touch.pageY / window.innerHeight * 360;
+  container.style.animation = "none";
+  container.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+  showLove();
+});
+
+// Event untuk touch end
+window.addEventListener('touchend', () => {
+  container.style.animation = "animate 45s linear infinite";
+});
+
+// Event tambahan untuk memastikan elemen love muncul saat disentuh
+container.addEventListener('touchstart', showLove);
